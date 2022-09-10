61c9e72e6a2940f4fed016bbfa5ba0a1a30b15e0
Veronica Carrillo-Marquez <vcarrillo-marquez@cainc.com>
2022-09-10 01:59:29 -0400

    build: fix workflow

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import merge from 'deepmerge'
 import {createBasicConfig} from '@open-wc/building-rollup'
 import typescript from '@rollup/plugin-typescript'
+import pkg from './package.json'
 
 const baseConfig = createBasicConfig()
 
 export default merge(baseConfig, {
   input: './src/index.ts',
-  output: [{file: './dist/index.js', format: 'esm', sourcemap: true}],
+  output: [{file: pkg.main, format: 'esm', sourcemap: true}],
   plugins: [typescript()],
 })
