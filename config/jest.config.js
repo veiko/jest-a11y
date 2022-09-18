756e3c074818fd83aac2dd458c0fd43fe735f655
Veronica Carrillo-Marquez <vcarrillo-marquez@cainc.com>
2022-09-17 22:31:45 -0400

    test: baseUrl in tests

--- config/jest.config.js
+++ config/jest.config.js
@@ -2,13 +2,14 @@ const path = require('path')
 const rootDir = path.resolve(__dirname, '../')
 
 module.exports = {
+  clearMocks: true,
+  moduleDirectories: ['node_modules', 'src'],
   rootDir,
+  setupFilesAfterEnv: ['<rootDir>/config/jest-setup.js'],
   transform: {
     '\\.[t]sx?$': 'ts-jest',
   },
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/'],
-  setupFilesAfterEnv: ['<rootDir>/config/jest-setup.js'],
   verbose: true,
-  clearMocks: true,
 }
