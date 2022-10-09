3059bc79c84462c933a5f56171a6f3723b448056
Veronica Carrillo-Marquez <vcarrillo-marquez@cainc.com>
2022-10-09 12:42:53 -0400

    feat(types): add type support

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,10 +1,18 @@
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
 import pkg from '../package.json'
 
-export default {
-  input: './src/index.ts',
-  output: [{ file: pkg.main, format: 'esm', sourcemap: true }],
-  plugins: [nodeResolve(), commonjs(), typescript()],
-}
+export default [
+  {
+    input: './src/index.ts',
+    output: [{ file: pkg.main, format: 'esm', sourcemap: true }],
+    plugins: [nodeResolve(), commonjs(), typescript()],
+  },
+  {
+    input: './src/types/global.d.ts',
+    output: [{ file: pkg.types, format: 'esm' }],
+    plugins: [dts()],
+  },
+]
