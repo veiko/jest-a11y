7d8dae3e4db6470af74eb723cf7cc5563fe890f8
Veronica Carrillo-Marquez <veiko@f1v.co>
2022-10-30 09:59:37 -0400

    fix: export jest types

--- src/types/jest.d.ts
+++ src/types/jest.d.ts
@@ -1,31 +1,39 @@
-declare namespace jest {
-  interface Matchers {
-    /**
-     * Assert whether an element has the correct role for alert.
-     *
-     * @example
-     * <div id="targetAlert" role="alert" />
-     *
-     * expect(screen.getByTestId('targetAlert')).toBeAccessibleAlert()
-     */
-    toBeAccessibleAlert(): CustomMatcherResult
-    /**
-     * Assert whether an element has the correct keyboard interaction, label and role for a button.
-     *
-     * @example
-     * <button id="targetBtn" />
-     *
-     * expect(screen.getByRole('button')).toBeAccessibleButton()
-     */
-    toBeAccessibleButton(): CustomMatcherResult
-    /**
-     * Assert whether an element has the correct keyboard interaction, label and role for a checkbox.
-     *
-     * @example
-     * <input aria-label="My Checkbox" type="checkbox" />
-     *
-     * expect(screen.getByRole('checkbox')).toBeAccessibleCheckbox()
-     */
-    toBeAccessibleCheckbox(): CustomMatcherResult
+interface CustomMatchers<CustomMatcherResult = unknown> {
+  /**
+   * Assert whether an element has the correct role for alert.
+   *
+   * @example
+   * <div id="targetAlert" role="alert" />
+   *
+   * expect(screen.getByTestId('targetAlert')).toBeAccessibleAlert()
+   */
+  toBeAccessibleAlert(): CustomMatcherResult
+  /**
+   * Assert whether an element has the correct keyboard interaction, label and role for a button.
+   *
+   * @example
+   * <button id="targetBtn" />
+   *
+   * expect(screen.getByRole('button')).toBeAccessibleButton()
+   */
+  toBeAccessibleButton(): CustomMatcherResult
+  /**
+   * Assert whether an element has the correct keyboard interaction, label and role for a checkbox.
+   *
+   * @example
+   * <input aria-label="My Checkbox" type="checkbox" />
+   *
+   * expect(screen.getByRole('checkbox')).toBeAccessibleCheckbox()
+   */
+  toBeAccessibleCheckbox(): CustomMatcherResult
+}
+
+declare global {
+  namespace jest {
+    interface Expect extends CustomMatchers {}
+    interface Matchers<R> extends CustomMatchers<R> {}
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
   }
 }
+
+export {}
