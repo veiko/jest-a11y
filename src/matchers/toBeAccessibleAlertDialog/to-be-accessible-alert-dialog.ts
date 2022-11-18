57f8b51fce540cc116bff3b3fb9616a7fcb597c4
Veronica Carrillo-Marquez <veiko@f1v.co>
2022-11-18 18:21:30 -0500

    fix(alertdialog): check aria-describedby #21

--- src/matchers/toBeAccessibleAlertDialog/to-be-accessible-alert-dialog.ts
+++ src/matchers/toBeAccessibleAlertDialog/to-be-accessible-alert-dialog.ts
@@ -1,18 +1,18 @@
 import userEvent from '@testing-library/user-event'
+import { assertAttribute } from 'utils/assertAttribute'
 import { assertLabel } from 'utils/assertLabel'
 import { assertRole } from 'utils/assertRole'
 import { printUtil } from 'utils/printUtil'
 
 /**
- * https://www.w3.org/WAI/ARIA/apg/patterns/dialogmodal/
+ * https://www.w3.org/WAI/ARIA/apg/patterns/alertdialog/
  *
  * WAI-ARIA Roles, States, and Properties
- * 1. The element that serves as the dialog container has role of dialog.
- * 2. All elements required to operate the dialog are descendants of the element that has role dialog.
- * 3. The dialog container element has aria-modal set to true.
- * 4. The dialog has either:
+ * 1. The element that serves as the dialog container has role of alertdialog.
+ * 2. The dialog has either:
  *   - A value set for the aria-labelledby property that refers to a visible dialog title.
  *   - A label specified by aria-label.
+ * 3. The element with role alertdialog has a value set for aria-describedby that refers to the element containing the alert message.
  *
  * Keyboard Interaction
  * 1. Tab moves focus to the next tabbable element inside the dialog. If focus is on the last tabbable element inside the dialog, moves focus to the first tabbable element inside the dialog.
@@ -23,18 +23,31 @@ export function toBeAccessibleAlertDialog(
   this: any,
   element: HTMLElement,
 ): jest.CustomMatcherResult {
-  const roleCheck = assertRole({ element, role: 'alertdialog', utils: this.utils })
-  if (!roleCheck?.pass) {
-    return roleCheck
-  }
-
-  const labelCheck = assertLabel({ element, utils: this.utils })
-  if (!labelCheck?.pass) {
-    return labelCheck
-  }
-
   let message = ''
   let pass = true
+
+  const roleCheck = assertRole({ element, role: 'alertdialog', utils: this.utils })
+  message += roleCheck.message()
+  pass = pass ? roleCheck.pass : pass
+
+  const labelCheck = assertLabel({ element, utils: this.utils })
+  message += labelCheck.message()
+  pass = pass ? labelCheck.pass : pass
+
+  const descriptionCheck = assertAttribute({
+    attribute: 'aria-describedby',
+    element,
+    utils: this.utils,
+  })
+  message += descriptionCheck.message()
+  pass = pass ? descriptionCheck.pass : pass
+
+  if (document.getElementById(element.getAttribute('aria-describedby') || '')) {
+    message += printUtil.pass('element description is present', { utils: this.utils })
+    pass = pass ? true : false
+  } else {
+    message += printUtil.fail('element description is present', { utils: this.utils })
+  }
 
   try {
     userEvent.keyboard('{esc}')
@@ -45,5 +58,5 @@ export function toBeAccessibleAlertDialog(
     pass = false
   }
 
-  return { pass, message: () => message }
+  return { message: () => message, pass }
 }
