eefbbe098a00deb5b4fddf72fe7faad1c35c277e
Veronica Carrillo-Marquez <veiko@f1v.co>
2022-11-26 09:57:17 -0500

    docs(List): add missing keys

--- docs/components/List.tsx
+++ docs/components/List.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const List = ({ items }) => {
   return (
     <ul className="list">
-      {items.map(item => {
+      {items.map((item, i) => {
         return (
-          <li>
+          <li key={`item-${i}`}>
             <div className="li-icon">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -14,9 +14,9 @@ export const List = ({ items }) => {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="var(--purple)"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <line x1="5" y1="12" x2="19" y2="12"></line>
                 <polyline points="12 5 19 12 12 19"></polyline>
