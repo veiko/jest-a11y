5c7a5135324fee9c961363970b607f63f9974ca2
Veronica Carrillo-Marquez <veiko@f1v.co>
2022-11-27 23:50:39 -0500

    wip

--- docs/components/ArrowPointer.tsx
+++ docs/components/ArrowPointer.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import { Arrow } from './Arrow'
 import { ExampleContext } from './ExampleContainer'
 
@@ -13,9 +13,36 @@ export const ArrowPointer = ({ color, size }) => {
     },
     tooltip,
   } = useContext(ExampleContext)
+
+  const arrowText = useRef<HTMLDivElement>(null)
+  // keep a mirror of the element so we can measure it
+  const mirrorText = useRef<HTMLDivElement>(null)
+  const [height, setHeight] = React.useState(0)
+
+  useEffect(() => {
+    const newHeight = mirrorText.current?.getBoundingClientRect()?.height || 0
+
+    // @see https://www.joshwcomeau.com/react/animating-the-unanimatable/#playing
+    requestAnimationFrame(() => {
+      arrowText.current!.style.maxHeight = `${height}px`
+      arrowText.current!.style.transition = 'none'
+
+      requestAnimationFrame(() => {
+        arrowText.current!.style.cssText = `max-height: ${newHeight}px; transition: ease-in-out 360ms max-height 0s;`
+      })
+    })
+    setHeight(newHeight)
+  }, [exampleText.text])
+
   return tooltip ? (
+    // @ts-ignore
     <div className="arrow-pointer" style={{ transform: `translateY(${exampleText.transformY}px)` }}>
-      <div className="arrow-text">{exampleText.text}</div>
+      <div className="arrow-text" key="my-arrow-text" ref={arrowText}>
+        {exampleText.text}
+      </div>
+      <div className="arrow-text" ref={mirrorText} style={{ visibility: 'hidden' }}>
+        {exampleText.text}
+      </div>
       <div>
         <Arrow color={color} size={size} />
       </div>
